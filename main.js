1dc9f0fb59462ab521a4f23b871ea9654175366b
Kobe Lovan <kobelovan7@gmail.com>
2022-02-23 07:14:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
-function displayChange() {
-    document.getElementById("display").innerHTML = "number"
+//addition
+function add(num1, num2) {
+    let res = num1 + num2;
+    window.alert(res);
+}
+
+//subtraction
+function sub(num1, num2) {
+    let res = num1 - num2;
+    window.alert(res);
+}
+
+//division
+function divide(num1, num2) {
+    let res = num1 / num2;
+    window.alert(res);
+}
+
+//multiplication
+function multiply(num1, num2) {
+    let res = num1 * num2;
+    console.log(res);
+}
+
+
+//determines which operator to execute
+function operate(op, num1, num2) {
+    if (op == "+") {
+        add(num1, num2);
+    } else if (op == "-") {
+        sub(num1, num2);
+    } else if (op == "/") {
+        divide(num1, num2);
+    } else if (op == "*") {
+        multiply(num1, num2);
+    } else {
+        console.log(" ");
+    }
 }
