dbb67f519751ce5d3b5f1f855b87e40df5c9fcaf
Kobe Lovan <kobelovan7@gmail.com>
2022-02-24 07:25:00 -0600

    Update main.js
    updated functions for operators

--- main.js
+++ main.js
@@ -1,72 +1,72 @@
 function num0() {
     document.getElementById("display").innerHTML = 0;
-  }
+}
 
 function num1() {
     document.getElementById("display").innerHTML = 1;
-  }
+}
 
-  function num2() {
+function num2() {
     document.getElementById("display").innerHTML = 2;
-  }
+}
 
 
-  function num3() {
+function num3() {
     document.getElementById("display").innerHTML = 3;
-  }
+}
 
 
-  function num4() {
+function num4() {
     document.getElementById("display").innerHTML = 4;
-  }
+}
 
 
-  function num5() {
+function num5() {
     document.getElementById("display").innerHTML = 5;
-  }
+}
 
 
-  function num6() {
+function num6() {
     document.getElementById("display").innerHTML = 6;
-  }
+}
 
 
-  function num7() {
+function num7() {
     document.getElementById("display").innerHTML = 7;
-  }
+}
 
 
-  function num8() {
+function num8() {
     document.getElementById("display").innerHTML = 8;
-  }
+}
 
 
-  function num9() {
+function num9() {
     document.getElementById("display").innerHTML = 9;
-  }
+}
 
 //addition 
 function add(num1, num2) {
     let res = num1 + num2;
-    window.alert(res);
+    document.getElementById("display").innerHTML = res;
 }
 
 //subtraction
 function sub(num1, num2) {
     let res = num1 - num2;
-    window.alert(res);
+    document.getElementById("display").innerHTML = res;
 }
 
 //division
 function divide(num1, num2) {
     let res = num1 / num2;
-    window.alert(res);
+    document.getElementById("display").innerHTML = res;
 }
 
 //multiplication
 function multiply(num1, num2) {
     let res = num1 * num2;
-    console.log(res);
+    document.getElementById("display").innerHTML = res;
 }
 
 
